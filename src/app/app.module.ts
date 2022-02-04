e3013b5a288b709d66a5049ee637fe876804cc83
mukund720 <mukund720@gmail.com>
2022-02-04 21:18:32 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AgmCoreModule } from '@agm/core';
+import { AgmDirectionModule } from 'agm-direction';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCr17JyBUkudCa6BTxniFNWPeADAGPXcIo'
-    }) 
+    }),
+    AgmDirectionModule 
   ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
